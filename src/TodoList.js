21b4768a78dc1577ea70a23b8e1c6d0b377944c2
unknown <ggq18663278150@sina.com>
2019-02-28 14:45:06 +0800

    redux

--- src/TodoList.js
+++ src/TodoList.js
@@ -1,29 +1,47 @@
 import React, { Component } from 'react'
 import "antd/dist/antd.css"
 import { Input, Button, List } from 'antd';
+import store from './store'
 
-const data = [
-  'Racing car sprays burning fuel into crowd.',
-  'Japanese princess to wed commoner.',
-  'Australian walks 100km after outback crash.',
-  'Man charged over missing wedding girl.',
-  'Los Angeles battles huge wildfires.',
-];
 export default class TodoList extends Component {
+  constructor() {
+    super();
+    console.log(store.getState())
+    this.state = store.getState();
+    store.subscribe(() => { this.handlerStoreChange() })
+  }
+
   render() {
     return (
       <div style={{ marginTop: '10px', marginLeft: '10px' }}>
         <div>
-          <Input placeholder="todo info" style={{ width: '300px', marginRight: '10px' }} />
-          <Button type='primary'>提交</Button>
+          <Input placeholder="todo info" style={{ width: '300px', marginRight: '10px' }} value={this.state.inputValue} onChange={(e) => this.handleOnChange(e)} />
+          <Button type='primary' onClick={()=>this.handleClick()}>提交</Button>
         </div>
         <List
-          style={{marginTop:'10px',width:'300px'}}
+          style={{ marginTop: '10px', width: '300px' }}
           bordered
-          dataSource={data}
+          dataSource={this.state.list}
           renderItem={item => (<List.Item>{item}</List.Item>)}
         />
       </div>
     )
+  }
+
+  handleOnChange(e) {
+    const action = {
+      type: 'change_input_value',
+      inputValue: e.target.value
+    }
+    store.dispatch(action)
+  }
+  handlerStoreChange() {
+      this.setState(store.getState())
+  }
+  handleClick(){
+    const action = {
+      type:"add_todo_item",
+    }
+    store.dispatch(action)
   }
 }
