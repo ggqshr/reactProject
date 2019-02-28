f2229834aeaeec3b0b5ef5514e429cec6f96c244
unknown <ggq18663278150@sina.com>
2019-02-28 09:26:02 +0800

    添加了ref引用的内容以及setState的回调函数

--- src/TodoList.js
+++ src/TodoList.js
@@ -30,10 +30,12 @@ class TodoList extends Component {
                         id="inputarea"
                         value={this.state.inputValue}
                         className='input'
-                        onChange={(e) => this.handleInputChange(e)} />
+                        onChange={(e) => this.handleInputChange(e)}
+                        ref={(input)=>{this.input = input}}
+                        />
                     <button onClick={() => this.handleButtonClick()}>提交</button>
                 </div>
-                <ul>
+                <ul ref={(ul)=>{this.ul = ul}}>
                     {this.getItem()}
                 </ul>
             </Fragment>
@@ -51,7 +53,10 @@ class TodoList extends Component {
         })
     }
     handleInputChange(e) {
-        let value = e.target.value;
+
+        //使用e.target的当时获取节点
+        // let value = e.target.value;
+        let value = this.input.value
         this.setState(() => {
             return {
                 inputValue: value
@@ -59,12 +64,17 @@ class TodoList extends Component {
         })
     }
     handleButtonClick() {
+        // setState是一个异步函数,第一个函数可以更新statue，第二个函数是一个回调函数，在执行完更新之后，可以回调一个函数
         this.setState((prev) => {
             return {
                 list: [...prev.list, prev.inputValue],
                 inputValue: ""
             }
-        })
+        },
+        ()=>{
+            console.log(this.ul.querySelectorAll('div').length)
+        }
+        )
     }
     handlerItemClick(index) {
         this.setState(
