dac821c49f3a8fcf37674656909e1790cb301599
unknown <ggq18663278150@sina.com>
2019-02-28 15:18:49 +0800

    拆分action

--- src/TodoList.js
+++ src/TodoList.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react'
 import "antd/dist/antd.css"
 import { Input, Button, List } from 'antd';
 import store from './store'
+import { CHANGE_INPUT_VALUE, ADD_TODO_ITEM, DELETE_TODO_ITEM } from './store/actionTypes'
+
 
 export default class TodoList extends Component {
   constructor() {
@@ -30,7 +32,7 @@ export default class TodoList extends Component {
 
   handleOnChange(e) {
     const action = {
-      type: 'change_input_value',
+      type: CHANGE_INPUT_VALUE,
       inputValue: e.target.value
     }
     store.dispatch(action)
@@ -40,13 +42,13 @@ export default class TodoList extends Component {
   }
   handleClick(){
     const action = {
-      type:"add_todo_item",
+      type:ADD_TODO_ITEM,
     }
     store.dispatch(action)
   }
   handleDelete(index){
       let action = {
-        type:'delete_todo_item',
+        type:DELETE_TODO_ITEM,
         index:index
       }
       store.dispatch(action)
--- src/store/reducer.js
+++ src/store/reducer.js
@@ -1,25 +1,25 @@
-
+import { CHANGE_INPUT_VALUE, ADD_TODO_ITEM, DELETE_TODO_ITEM } from './actionTypes'
 const defaultState = {
-    inputValue:"123",
-    list:[1,2]
+    inputValue: "123",
+    list: [1, 2]
 }
 
 
 export default (state = defaultState, action) => {
-    if(action.type === 'change_input_value'){
+    if (action.type === CHANGE_INPUT_VALUE) {
         const newState = JSON.parse(JSON.stringify(state))
         newState.inputValue = action.inputValue;
         return newState
     }
-    else if(action.type === 'add_todo_item'){
+    else if (action.type === ADD_TODO_ITEM) {
         const newState = JSON.parse(JSON.stringify(state))
         newState.list.push(newState.inputValue)
         newState.inputValue = ""
         return newState
     }
-    else if(action.type === "delete_todo_item"){
+    else if (action.type === DELETE_TODO_ITEM) {
         const newState = JSON.parse(JSON.stringify(state))
-        newState.list.splice(action.index,1)
+        newState.list.splice(action.index, 1)
         return newState;
     }
     return state;
