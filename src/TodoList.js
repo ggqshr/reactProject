f1b3185974931b873f3fe55fe211244b037de736
unknown <ggq18663278150@sina.com>
2019-03-01 11:03:22 +0800

    将组件变成函数

--- src/TodoList.js
+++ src/TodoList.js
@@ -1,38 +1,63 @@
-import React, { Component } from 'react'
+import React from 'react'
 import { connect } from 'react-redux'
 import { changeInputValueAction, addItemAction, deleteItemAction } from './store/actionCreater'
 
 
-
-class TodoList extends Component {
-    render() {
-        const { inputValue,ChangeInputvalue,handleClick,handleDelete } = this.props;
-        return (
+const TodoList = (props) => {
+    const { inputValue, ChangeInputvalue, handleClick, handleDelete } = props;
+    return (
+        <div>
             <div>
-                <div>
-                    <input value={inputValue} onChange={ChangeInputvalue} />
-                    <button onClick={handleClick}>提交</button>
-                </div>
-                <ul>
-                    {
-                        this.props.list.map((item, index) => {
-                            return (
-                                <li key={index} onClick={()=>handleDelete(index)}>{item}</li>
-                            )
-                        })
-                    }
-                </ul>
+                <input value={inputValue} onChange={ChangeInputvalue} />
+                <button onClick={handleClick}>提交</button>
             </div>
-        )
-    }
+            <ul>
+                {
+                    props.list.map((item, index) => {
+                        return (
+                            <li key={index} onClick={() => handleDelete(index)}>{item}</li>
+                        )
+                    })
+                }
+            </ul>
+        </div>
+    )
 }
+
+
+// class TodoList extends Component {
+//     render() {
+//         const { inputValue, ChangeInputvalue, handleClick, handleDelete } = this.props;
+//         return (
+//             <div>
+//                 <div>
+//                     <input value={inputValue} onChange={ChangeInputvalue} />
+//                     <button onClick={handleClick}>提交</button>
+//                 </div>
+//                 <ul>
+//                     {
+//                         this.props.list.map((item, index) => {
+//                             return (
+//                                 <li key={index} onClick={() => handleDelete(index)}>{item}</li>
+//                             )
+//                         })
+//                     }
+//                 </ul>
+//             </div>
+//         )
+//     }
+// }
 //连接的规则
+
+
 const mapStateToProps = (state) => {
     return {
         inputValue: state.inputValue,
         list: state.list
     }
 }
+
+
 const mapDispatchToProps = (dispatch) => {
     return {
         ChangeInputvalue(e) {
@@ -43,7 +68,7 @@ const mapDispatchToProps = (dispatch) => {
             const action = addItemAction()
             dispatch(action)
         },
-        handleDelete(index){
+        handleDelete(index) {
             const action = deleteItemAction(index)
             dispatch(action)
         }
